61357438ff21d1d39a3763d76e115e5a07227c7b
juniorfelix998 <juniorfelix998@gmail.com>
2021-02-25 09:26:45 +0300

    Added the backend with node to fetch Products

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Footer = () => {
+    return (
+        <Container>
+<footer>
+            <Row>
+                <Col className="text-center py-3">
+                    Copyright &copy; ProShop
+                </Col>
+            </Row>
+            
+        </footer>
+        </Container>
+        
+    )
+}
+
+export default Footer
